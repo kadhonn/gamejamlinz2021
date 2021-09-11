b537015ae9f45205aab6cae38cc3621485f36df6
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2021-09-11 16:07:22 +0200

    tile everything

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -14,36 +14,32 @@ export class GameScene extends Scene {
     }
 
     create() {
-        //  A simple background for our game
-        this.add.tileSprite(400, 300, 0, 0, 'sky');
+        this.physics.world.setBounds(0, 0, 2400, 600)
 
-        //  The platforms group contains the ground and the 2 ledges we can jump on
+        for (let i = 0; i < 10; i++) {
+            this.add.image(i * 800, 300, 'sky');
+        }
+
         this.platforms = this.physics.add.staticGroup();
-
-        //  Here we create the ground.
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-        //  Now let's create some ledges
+        for (let i = 0; i < 10; i++) {
+            this.platforms.create(400 + i * 800, 568, 'ground').setScale(2).refreshBody();
+        }
         this.platforms.create(600, 400, 'ground');
         this.platforms.create(50, 250, 'ground');
         this.platforms.create(750, 220, 'ground');
 
-        // The player and its settings
         this.player = this.physics.add.sprite(100, 450, 'moss');
 
-        //  Player physics properties. Give the little guy a slight bounce.
         this.player.setCollideWorldBounds(true);
 
-        //  Collide the player and the stars with the platforms
-        this.physics.add.collider(this.player, this.platforms)
+        this.physics.add.collider(this.player, this.platforms);
     }
 
     update() {
         if (this.gameOver) {
             return;
         }
-        this.player.setVelocityX(140);
+        this.player.setVelocityX(150);
         this.cameras.main.scrollX = this.player.x - 200;
     }
 
