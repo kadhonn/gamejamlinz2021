84a56e683ec915060bee4b3f6c1b93ed4436ed32
Pia Gerhofer <pia.gerhofer@teamecho.at>
2021-09-11 18:26:23 +0200

    fix jen animation, add roy animation, add differentiation between obstacles and ground

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -5,12 +5,14 @@ export class GameScene extends Scene {
     player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     follower: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     platforms: Phaser.Physics.Arcade.StaticGroup;
+    obstacles: Phaser.Physics.Arcade.StaticGroup;
     gameOver = false;
     cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
     preload() {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('ground', 'assets/platform.png');
+        this.load.image('moss', 'assets/moss.jpg');
         this.load.spritesheet('roy', 'assets/roy.png', {frameWidth: 20, frameHeight: 39});
         this.load.spritesheet('jen', 'assets/jen.png', {frameWidth: 25, frameHeight: 51});
     }
@@ -22,16 +24,15 @@ export class GameScene extends Scene {
             this.add.image(i * 800, 300, 'sky');
         }
 
+        this.obstacles = this.physics.add.staticGroup();
+        this.obstacles.create(600, 400, 'moss');
+
         this.platforms = this.physics.add.staticGroup();
         for (let i = 0; i < 10; i++) {
             this.platforms.create(400 + i * 800, 568, 'ground').setScale(2).refreshBody();
         }
-        this.platforms.create(600, 400, 'ground');
-        this.platforms.create(50, 250, 'ground');
-        this.platforms.create(750, 220, 'ground');
 
         // ROY SETUP
-
         this.player = this.physics.add.sprite(100, 450, 'roy').setScale(4);
         this.player.setCollideWorldBounds(true);
 
@@ -44,12 +45,16 @@ export class GameScene extends Scene {
 
         this.player.anims.create({
             key: 'shrug',
-            frames: this.anims.generateFrameNumbers('roy', {start: 0, end: 2}),
+            frames: this.anims.generateFrameNumbers('roy', {start: 0, end: 1}),
             frameRate: 8,
             repeat: -1,
         });
 
+        this.player.anims.play('right', true);
         this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.player, this.obstacles, () => {
+            this.player.anims.play('shrug', true);
+        });
 
         // JEN SETUP
         this.follower = this.physics.add.sprite(400, 320, 'jen').setScale(2);
@@ -81,7 +86,6 @@ export class GameScene extends Scene {
             return;
         }
         this.player.setVelocityX(150);
-        this.player.anims.play('right', true);
 
         let oldCameraScrollX = this.cameras.main.scrollX;
         this.cameras.main.scrollX = this.player.x - 200;
@@ -94,16 +98,17 @@ export class GameScene extends Scene {
         let screenFollowerX = this.follower.x - this.cameras.main.scrollX;
         let screenFollowerY = this.follower.y - this.cameras.main.scrollY;
 
-        let deltaX = (this.input.activePointer.x - screenFollowerX) / 10;
-        let deltaY = (this.input.activePointer.y - screenFollowerY) / 10;
-
-        if (this.cursors.down.isDown) {
-            this.follower.anims.play("cry", true);
-        } else if (deltaX > 0) {
+        let xOffset = 0;
+        if (this.input.activePointer.x > screenFollowerX) {
+            xOffset = -20;
             this.follower.anims.play("right", true);
         } else {
             this.follower.anims.play("left", true)
+            xOffset = 20;
         }
+
+        let deltaX = ((this.input.activePointer.x + xOffset) - screenFollowerX) / 10;
+        let deltaY = ((this.input.activePointer.y + 30) - screenFollowerY) / 10;
 
         this.follower.x = screenFollowerX + deltaX + this.cameras.main.scrollX;
         this.follower.y = screenFollowerY + deltaY + this.cameras.main.scrollY;
