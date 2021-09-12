e988a1185d21a26c78ff02c2a3e3e8862febf806
Pia Gerhofer <pia.gerhofer@teamecho.at>
2021-09-12 20:00:41 +0200

    add bug & feature count

--- src/sprites/obstacles/internet.ts
+++ src/sprites/obstacles/internet.ts
@@ -1,10 +1,18 @@
 import {GameScene, SCALE} from "../../scenes/gameScene";
 import TimerEvent = Phaser.Time.TimerEvent;
 
+let bugsCaughtText;
+let bugsLostText;
+let featuresText;
+
 export function setupInternet(scene: GameScene, x: number) {
     const moss = scene.physics.add.staticSprite(800 + x, 465, 'moss')
         .setScale(SCALE)
         .refreshBody();
+
+    bugsCaughtText = scene.add.text(x + 200, 140, 'bugs caught: 0', { fontSize: '32px'});
+    bugsLostText = scene.add.text(x + 200, 170, 'bugs gotten: 0', { fontSize: '32px'});
+    featuresText = scene.add.text(x + 200, 200, 'features: 0', { fontSize: '32px' });
 
     const theInternet = scene.physics.add.staticSprite(700 + x, 450, 'theInternet')
         .setScale(SCALE + 1)
@@ -46,6 +54,9 @@ export function setupInternet(scene: GameScene, x: number) {
 
 const features = [];
 const bugs = [];
+let bugsReceived = 0;
+let bugsAvoided = 0;
+let featuresCaught = 0;
 
 function generateElement(scene: GameScene, xStart: number) {
     const xPosition = xStart + 200 + Math.random() * 1000;
@@ -54,14 +65,47 @@ function generateElement(scene: GameScene, xStart: number) {
     const element = scene.physics.add.sprite(xPosition, -20, type)
         .setScale(SCALE)
         .setVelocity(-50 + Math.random() * 100, 100)
+        .setInteractive()
         .refreshBody();
     element.body.setAllowGravity(false);
 
     if (type === 'bug' || type === 'bug2' || type === 'virus') {
         bugs.push(element)
+        element.once('pointerup', (pointer) => {
+            bugsAvoided++;
+            bugsCaughtText.setText(`bugs caught: ${bugsAvoided}`);
+            console.log("catched a bug!");
+            const index = bugs.indexOf((bug) => bug === element);
+            bugs.splice(index, 1);
+            element.destroy();
+        });
     } else {
         features.push(element);
+        element.once('pointerup', (pointer) => {
+            featuresCaught++;
+            featuresText.setText(`features: ${featuresCaught}`);
+            console.log("added a feature!");
+            const index = features.indexOf((feature) => feature === element);
+            features.splice(index, 1);
+            element.destroy();
+        });
     }
+
+    scene.time.addEvent(new TimerEvent({
+        delay: 500,
+        repeat: -1,
+        loop: true,
+        callback: () => {
+            for (const idx in bugs) {
+                if (bugs[idx].y > 600) {
+                    bugsReceived++;
+                    bugsLostText.setText(`bugs gotten: ${bugsReceived}`);
+                    bugs[idx].destroy();
+                    bugs.splice(Number(idx), 1);
+                }
+            }
+        }
+    }))
 }
 
 function getRandomType() {
